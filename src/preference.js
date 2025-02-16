32e047784b2d2d18bdd717473202065e95fb8c2f
Izik Avinoam <izika@eyenet-mobile.com>
2025-02-16 22:12:12 +0200

    rename JS file and replace coockie withe localStorage

---
+++ src/preference.js
@@ -0,0 +1,22 @@
+function applyTheme() {
+    const theme = localStorage.getItem("theme") || "light";
+    if (theme === "dark") {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+}
+
+function toggleTheme() {
+    const theme = localStorage.getItem("theme") || "light";
+    if (theme === "dark") {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
+    applyTheme();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    applyTheme();
+});
