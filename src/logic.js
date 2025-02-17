d5723667157635aba6086827f8705800cd95ea3b
Izik Avinoam <izika@eyenet-mobile.com>
2025-02-17 22:02:08 +0200

    Attempt to allow only Hebrew text

--- src/logic.js
+++ src/logic.js
@@ -4,3 +4,24 @@ function clearAll() {
         inputsArray[i].value = "";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let inputsElements = document.getElementsByTagName("input");
+    for (let i = 0; i < inputsElements.length; i++) {
+        inputsElements[i].addEventListener("keydown", function(event) {
+            let char = event.key;
+            let val = event.value;
+            const regex = /[\u0590-\u05fe]/;
+            console.log(" * " + char + " test: " + regex.test(char));
+            console.log(this);
+        
+            if (regex.test(char)) {
+                console.log("hebrew");
+            } else {
+                console.log("not hebrew: " + char);
+                this.value = "";
+            }
+            console.log("value: " + this.value);
+        });
+    }
+});
